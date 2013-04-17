64078b3aea997fa40d6e9a8807e7c0c222573575
Walter Heck <walterheck@olindata.com>
2013-04-17 15:42:44 +0200

    re-add puppet runstyle for cron

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -10,20 +10,21 @@
 #
 # Sample Usage:
 #
-class puppet::agent(
+class puppet::agent (
   $puppet_server,
-  $environment = $::puppet::params::environment,
-  $puppet_defaults = $::puppet::params::puppet_defaults,
+  $environment          = $::puppet::params::environment,
+  $puppet_defaults      = $::puppet::params::puppet_defaults,
   $puppet_agent_service = $::puppet::params::puppet_agent_service,
-  $puppet_agent_name = $::puppet::params::puppet_agent_name,
-  $puppet_conf = $::puppet::params::puppet_conf,
-  $package_provider = undef,
-  $reporting = true,
-  $version = 'present',
-  $user_id = undef,
-  $group_id = undef,
-) inherits puppet::params {
-
+  $puppet_agent_name    = $::puppet::params::puppet_agent_name,
+  $puppet_conf          = $::puppet::params::puppet_conf,
+  $package_provider     = undef,
+  $reporting            = true,
+  $version              = 'present',
+  $puppet_agent_enabled = true,
+  $puppet_run_style     = 'service',
+  $puppet_run_interval  = 30,
+  $user_id              = undef,
+  $group_id             = undef,) inherits puppet::params {
   class { 'puppet::common':
     user_id  => $user_id,
     group_id => $group_id,
@@ -38,42 +39,74 @@ class puppet::agent(
     }
   }
 
-  if ! defined(Package[$puppet_agent_name]) {
+  if !defined(Package[$puppet_agent_name]) {
     package { $puppet_agent_name:
       ensure   => $version,
       provider => $package_provider,
     }
   }
 
-  if $package_provider == 'gem' {
-    $service_notify = Exec['puppet_agent_start']
+  case $puppet_run_style {
+    'service' : {
+      if $package_provider == 'gem' {
+        $service_notify = Exec['puppet_agent_start']
 
-    exec { 'puppet_agent_start':
-      command   => '/usr/bin/nohup puppet agent &',
-      refresh   => '/usr/bin/pkill puppet && /usr/bin/nohup puppet agent &',
-      unless    => "/bin/ps -ef | grep -v grep | /bin/grep 'puppet agent'",
-      require   => File[$puppet_conf],
-      subscribe => Package[$puppet_agent_name],
+        exec { 'puppet_agent_start':
+          command   => '/usr/bin/nohup puppet agent &',
+          refresh   => '/usr/bin/pkill puppet && /usr/bin/nohup puppet agent &',
+          unless    => "/bin/ps -ef | grep -v grep | /bin/grep 'puppet agent'",
+          require   => File[$puppet_conf],
+          subscribe => Package[$puppet_agent_name],
+        }
+      } else {
+        $service_notify = Service[$puppet_agent_service]
+
+        service { $puppet_agent_service:
+          ensure     => running,
+          enable     => true,
+          hasstatus  => true,
+          hasrestart => true,
+          require    => File[$puppet_conf],
+          subscribe  => Package[$puppet_agent_name],
+        }
+      }
+
     }
-  } else {
-    $service_notify = Service[$puppet_agent_service]
+    'cron'    : {
+      # ensure that puppet is running and will start up on boot
+      service { $puppet_agent_service:
+        ensure     => 'stopped',
+        enable     => false,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Package[$puppet_agent_name],
+      }
 
-    service { $puppet_agent_service:
-      ensure     => running,
-      enable     => true,
-      hasstatus  => true,
-      hasrestart => true,
-      require    => File[$puppet_conf],
-      subscribe  => Package[$puppet_agent_name],
+      # Run puppet as a cron - this saves memory and avoids the whole problem
+      # where puppet locks up for no reason. Also spreads out the run intervals
+      # more uniformly.
+      $time1 = fqdn_rand($puppet_run_interval)
+      $time2 = fqdn_rand($puppet_run_interval) + 30
+
+      cron { 'puppet-client':
+        command => '/usr/sbin/puppet agent --no-daemonize --onetime --logdest syslog > /dev/null 2>&1',
+        user    => 'root',
+        # run twice an hour, at a random minute in order not to collectively stress the puppetmaster
+        hour    => '*',
+        minute  => [$time1, $time2],
+      }
+    }
+    default   : {
+      err("Unsupported puppet run style in Class[\'puppet::agent\']")
     }
   }
 
-  File [$confdir] {
+  File[$confdir] {
     require +> Package[$puppet_agent_name],
     notify  +> $service_notify
   }
 
-  Concat<| title == $puppet_conf |> {
+  Concat <| title == $puppet_conf |> {
     require => Package['puppet'],
     notify  +> $puppet::agent::service_notify,
   }
